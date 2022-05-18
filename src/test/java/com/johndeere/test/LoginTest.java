295ddebf352671fba1d9a89a22257b2397e73355
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-05-18 12:12:31 +0530

    dataprovider for valid credential

--- src/test/java/com/johndeere/test/LoginTest.java
+++ src/test/java/com/johndeere/test/LoginTest.java
@@ -3,18 +3,37 @@ package com.johndeere.test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.johndeere.base.WebDriverWrapper;
-public class LoginTest extends WebDriverWrapper {
-	
-	//create dataprovider 
-//	admin, pass, English (Indian), OpenEMR
-//	physician, physician, English (Indian), OpenEMR
-//	accountant, accountant, English (Indian), OpenEMR
 
-	@Test
-	public void validCredentialTest(String username,String password,String language,String expectedTitle) {
+public class LoginTest extends WebDriverWrapper {
+
+	@DataProvider
+	public Object[][] validCredentialData() {
+		Object[][] main = new Object[3][4];
+
+		main[0][0] = "admin";
+		main[0][1] = "pass";
+		main[0][2] = "English (Indian)";
+		main[0][3] = "OpenEMR";
+
+		main[1][0] = "accountant";
+		main[1][1] = "accountant";
+		main[1][2] = "Danish";
+		main[1][3] = "OpenEMR";
+
+		main[2][0] = "physician";
+		main[2][1] = "physician";
+		main[2][2] = "English (Indian)";
+		main[2][3] = "OpenEMR";
+
+		return main;
+	}
+
+	@Test(dataProvider = "validCredentialData")
+	public void validCredentialTest(String username, String password, String language, String expectedTitle) {
 
 		driver.findElement(By.id("authUser")).sendKeys(username);
 		driver.findElement(By.id("clearPass")).sendKeys(password);
