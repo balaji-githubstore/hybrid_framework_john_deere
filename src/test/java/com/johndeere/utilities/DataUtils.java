5435bbbf3091cd37bd7c79e6530b0a22eca227a4
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-05-18 15:13:59 +0530

    valid credential - connected with excel

--- src/test/java/com/johndeere/utilities/DataUtils.java
+++ src/test/java/com/johndeere/utilities/DataUtils.java
@@ -14,24 +14,8 @@ public class DataUtils {
 	}
 
 	@DataProvider
-	public Object[][] validCredentialData() {
-		Object[][] main = new Object[3][4];
-
-		main[0][0] = "admin";
-		main[0][1] = "pass";
-		main[0][2] = "English (Indian)";
-		main[0][3] = "OpenEMR";
-
-		main[1][0] = "accountant";
-		main[1][1] = "accountant";
-		main[1][2] = "Danish";
-		main[1][3] = "OpenEMR";
-
-		main[2][0] = "physician";
-		main[2][1] = "physician";
-		main[2][2] = "English (Indian)";
-		main[2][3] = "OpenEMR";
-
+	public Object[][] validCredentialData() throws IOException {
+		Object[][] main= ExcelUtils.getSheetIntoTwoDimArray("test_data/open_emr_test_data.xlsx", "validCredentialTest");
 		return main;
 	}
 
