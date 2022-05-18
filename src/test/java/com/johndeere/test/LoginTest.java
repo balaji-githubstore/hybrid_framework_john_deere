fccf65d80da9f2c919cb0b8e17431e877fff7461
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-05-18 15:33:00 +0530

    commondataprovider

--- src/test/java/com/johndeere/test/LoginTest.java
+++ src/test/java/com/johndeere/test/LoginTest.java
@@ -10,7 +10,7 @@ import com.johndeere.utilities.DataUtils;
 
 public class LoginTest extends WebDriverWrapper {
 	
-	@Test(dataProviderClass = DataUtils.class,dataProvider = "validCredentialData")
+	@Test(dataProviderClass = DataUtils.class,dataProvider = "commonDataProvider")
 	public void validCredentialTest(String username, String password, String language, String expectedTitle) {
 
 		driver.findElement(By.id("authUser")).sendKeys(username);
@@ -24,7 +24,7 @@ public class LoginTest extends WebDriverWrapper {
 		Assert.assertEquals(actualTitle, expectedTitle);
 	}
 
-	@Test(dataProviderClass = DataUtils.class,dataProvider = "invalidCredentialData")
+	@Test(dataProviderClass = DataUtils.class,dataProvider = "commonDataProvider")
 	public void invalidCredentialTest(String username, String password, String language, String expectedError) {
 		driver.findElement(By.id("authUser")).sendKeys(username);
 		driver.findElement(By.id("clearPass")).sendKeys(password);
--- src/test/java/com/johndeere/utilities/DataUtils.java
+++ src/test/java/com/johndeere/utilities/DataUtils.java
@@ -1,22 +1,37 @@
 package com.johndeere.utilities;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 
 import org.testng.annotations.DataProvider;
 
 public class DataUtils {
 	
+//	@DataProvider
+//	public Object[][] invalidCredentialData() throws IOException
+//	{
+//		Object[][] main=ExcelUtils.getSheetIntoTwoDimArray("test_data/open_emr_test_data.xlsx", "invalidCredentialTest");
+//		return main;
+//	}
+//
+//	@DataProvider
+//	public Object[][] validCredentialData() throws IOException {
+//		Object[][] main= ExcelUtils.getSheetIntoTwoDimArray("test_data/open_emr_test_data.xlsx", "validCredentialTest");
+//		return main;
+//	}
+	
 	@DataProvider
-	public Object[][] invalidCredentialData() throws IOException
-	{
-		Object[][] main=ExcelUtils.getSheetIntoTwoDimArray("test_data/open_emr_test_data.xlsx", "invalidCredentialTest");
-		return main;
-	}
-
-	@DataProvider
-	public Object[][] validCredentialData() throws IOException {
-		Object[][] main= ExcelUtils.getSheetIntoTwoDimArray("test_data/open_emr_test_data.xlsx", "validCredentialTest");
+	public Object[][] commonDataProvider(Method method) throws IOException {
+		
+		String testMethodName=method.getName();
+		Object[][] main= ExcelUtils.getSheetIntoTwoDimArray("test_data/open_emr_test_data.xlsx", testMethodName);
 		return main;
 	}
 
 }
+
+
+
+
+
+
