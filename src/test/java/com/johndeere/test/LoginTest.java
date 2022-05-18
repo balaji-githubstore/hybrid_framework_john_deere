cad3146a37a10e7cf78a86679e9d0fc89213ed62
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-05-18 10:44:08 +0530

    before and after method annotation

--- src/test/java/com/johndeere/test/LoginTest.java
+++ src/test/java/com/johndeere/test/LoginTest.java
@@ -7,61 +7,54 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest {
-	
-	@Test
-	public void validCredentialTest()
-	{
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setup() {
 		WebDriverManager.chromedriver().setup();
-		
-		WebDriver driver=new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-		
 		driver.get("http://demo.openemr.io/b/openemr");
-		
+	}
+
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
+	}
+
+	@Test
+	public void validCredentialTest() {
+
 		driver.findElement(By.id("authUser")).sendKeys("admin");
 		driver.findElement(By.id("clearPass")).sendKeys("pass");
-		Select selectLan=new Select(driver.findElement(By.xpath("//select[@name='languageChoice']")));
-		selectLan.selectByVisibleText("English (Indian)");	
+		Select selectLan = new Select(driver.findElement(By.xpath("//select[@name='languageChoice']")));
+		selectLan.selectByVisibleText("English (Indian)");
 		driver.findElement(By.cssSelector("#login-button")).click();
-		
-		//wait until navigate to openemr dashboard
-		String actualTitle=driver.getTitle();
+
+		// wait until navigate to openemr dashboard
+		String actualTitle = driver.getTitle();
 		Assert.assertEquals(actualTitle, "OpenEMR");
 	}
-	
+
 	@Test
-	public void invalidCredentialTest()
-	{
-		WebDriverManager.chromedriver().setup();
-		
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-		
-		driver.get("http://demo.openemr.io/b/openemr");
-		
+	public void invalidCredentialTest() {
+
 		driver.findElement(By.id("authUser")).sendKeys("admin123");
 		driver.findElement(By.id("clearPass")).sendKeys("pass");
-		Select selectLan=new Select(driver.findElement(By.xpath("//select[@name='languageChoice']")));
-		selectLan.selectByVisibleText("English (Indian)");	
+		Select selectLan = new Select(driver.findElement(By.xpath("//select[@name='languageChoice']")));
+		selectLan.selectByVisibleText("English (Indian)");
 		driver.findElement(By.cssSelector("#login-button")).click();
-		
-		String actualError= driver.findElement(By.xpath("//*[contains(text(),'Invalid')]")).getText();
-		
+
+		String actualError = driver.findElement(By.xpath("//*[contains(text(),'Invalid')]")).getText();
+
 		Assert.assertEquals(actualError, "Invalid username or password");
 	}
 }
-
-
-
-
-
-
-
-
