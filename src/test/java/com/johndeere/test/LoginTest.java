8c4b32b283dfab0531ae30c711ed9d3772df8e62
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-05-18 10:01:27 +0530

    valid credential test method done

--- src/test/java/com/johndeere/test/LoginTest.java
+++ src/test/java/com/johndeere/test/LoginTest.java
@@ -2,8 +2,11 @@ package com.johndeere.test;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -14,20 +17,32 @@ public class LoginTest {
 	public void validCredentialTest()
 	{
 		WebDriverManager.chromedriver().setup();
+		
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		
 		driver.get("http://demo.openemr.io/b/openemr");
 		
-		//enter username as admin
-		//enter password as pass
-		//langugae as English (Indian)
-		//click on login
+		driver.findElement(By.id("authUser")).sendKeys("admin");
+		driver.findElement(By.id("clearPass")).sendKeys("pass");
 		
-		//assert the title 
+		Select selectLan=new Select(driver.findElement(By.xpath("//select[@name='languageChoice']")));
+		selectLan.selectByVisibleText("English (Indian)");
+		
+		driver.findElement(By.cssSelector("#login-button")).click();
+		
+		//wait until navigate to openemr dashboard
+		
+		String actualTitle=driver.getTitle();
+		Assert.assertEquals(actualTitle, "OpenEMR");
 	}
-	
-	
-
 }
+
+
+
+
+
+
+
+
